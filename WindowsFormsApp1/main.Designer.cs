da702443f433363e64b642768aa5044429d14d77
Дмитрия Титов <Friz German@DESKTOP-IT>
2021-12-09 12:37:51 +0500

    настройка переходов между формами

--- WindowsFormsApp1/main.Designer.cs
+++ WindowsFormsApp1/main.Designer.cs
@@ -37,7 +37,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 200);
+            this.button1.Location = new System.Drawing.Point(12, 35);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(211, 48);
             this.button1.TabIndex = 0;
@@ -46,7 +46,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 146);
+            this.button2.Location = new System.Drawing.Point(12, 89);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(211, 48);
             this.button2.TabIndex = 1;
@@ -55,7 +55,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 92);
+            this.button3.Location = new System.Drawing.Point(12, 143);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(211, 48);
             this.button3.TabIndex = 2;
@@ -65,7 +65,7 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button4.Location = new System.Drawing.Point(12, 38);
+            this.button4.Location = new System.Drawing.Point(12, 197);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(211, 48);
             this.button4.TabIndex = 3;
--- WindowsFormsApp1/main.cs
+++ WindowsFormsApp1/main.cs
@@ -27,30 +27,34 @@ namespace WindowsFormsApp1
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)//3автосалон
         {
-
+            _1avtosalon Avtosalon = new _1avtosalon();
+            Avtosalon.ShowDialog();
         }
 
         private void button4_Click(object sender, EventArgs e)// reginas
         {
-
+            _4avtosalon Avtosalon = new _4avtosalon();
+            Avtosalon.ShowDialog();
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)//бдшка
         {
             Form2 form2 = new Form2();
             form2.ShowDialog();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)//2автосалон
         {
-
+            _3avtosalon Avtosalon = new _3avtosalon();
+            Avtosalon.ShowDialog();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)//3автосалон
         {
-
+            _2avtosalon Avtosalon = new _2avtosalon();
+            Avtosalon.ShowDialog();
         }
     }
 }
