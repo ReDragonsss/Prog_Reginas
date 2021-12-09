67ff66604a2a93df785aa9c5e90f8be704670b71
Дмитрия Титов <Friz German@DESKTOP-IT>
2021-12-09 12:20:18 +0500

    save

---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string windows = textBox1.Text;
+            string operativka = textBox2.Text;
+            string sql = $"INSERT INTO bd_kompykter (vers_wind, kolvo_operativ)  VALUES ('{windows}','{operativka}');";// запрос на дбавление параметров
+            //try// правильность подключ
+            //{
+            //    connn.Open();
+            //    MessageBox.Show("Данные занесены в базу данных");
+            //    MySqlDataAdapter IDataAdapter = new MySqlDataAdapter(sql, conn);
+            //    connn.Close();
+            //}
+            //catch (Exception osh)
+            //{
+            //    MessageBox.Show("Произошла ошибка" + osh);
+            //    connn.Close();
+            //}
+        }
+    }
+}
